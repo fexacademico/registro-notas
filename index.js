83403f9b95da6a05688e8efe5aca015da4d7be0e
Felix Lee Pan <felix.leepan@windriver.com>
2025-04-01 15:16:53 -0600

    feat: add grades registry project html design and its corresponding files.

---
+++ index.js
@@ -0,0 +1,40 @@
+// Copyright © 2025 Felix Lee Pan
+// All rights reserved. Unauthorized copying, modification, or distribution of this software is prohibited.
+
+// TODO: Add DOM content loaded
+
+// TODO: Data Storage
+
+function trackSession() {
+
+}
+
+function checkUser() {
+
+}
+
+function setCookie() {
+
+}
+
+function getCookie() {
+
+}
+
+// TODO: Grade functionalities
+
+function addGrade() {
+
+}
+
+function saveGrade(name, grade) {
+
+}
+
+function loadGrades() {
+
+}
+
+function removeGrade(button) {
+
+}
